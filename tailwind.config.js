c0e45486ddca5bc8e5eb0097671e344f6c755775
Shivrajsinh Zala <zalashivraj2642003@gmail.com>
2024-12-24 20:54:13 +0530

    navigation panel added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

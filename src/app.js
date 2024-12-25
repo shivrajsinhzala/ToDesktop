2abb9486c003c6fa7bab87de5140c6beec196baa
Shivrajsinh Zala <zalashivraj2642003@gmail.com>
2024-12-26 03:37:07 +0530

    added mobile navigation responsiveness

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  document.getElementById("menuIcon").classList.toggle("hidden");
+  document.getElementById("closeIcon").classList.toggle("hidden");
+  document.getElementById("mobileMenu").classList.toggle("hidden");
+  document.getElementById("mobileMenu").classList.toggle("flex");
+}
+
+// function toggleMenu() {
+//   document.getElementById("some").classList.toggle("hidden");
+// }
